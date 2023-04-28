748dd424ffcde9effc96b7bfda6721dbee1ac4e3
tmq <1738227095@qq.com>
2023-04-28 17:43:39 +0800

    连接安卓手机配置

--- PytestAndFixtrue/andriod.py
+++ PytestAndFixtrue/andriod.py
@@ -6,13 +6,15 @@ Project：python学习
 =====================
 """
 from appium.webdriver import Remote
+from selenium.webdriver.common.by import By
 
 caps = {
     'platformName': 'Android',  # 平台
-    'deviceName': 'BRNUT21B15032048',  # 设备名 在cmd输入adb devices 可查看
-    'app': r'D:\androidsdk\android-sdk-windows\build-tools\app-debug.apk'  # 安装包路径
+    'deviceName': 'd86bb73c',  # 设备名 在cmd输入adb devices 可查看
+    # 'app': r'D:\androidsdk\android-sdk-windows\build-tools\app-debug.apk',  # 安装包路径
+    'noReset': True
 }
-driver = Remote(command_executor='http://127.0.0.1:4444/wd/hub', desired_capabilities=caps)
+driver = Remote(command_executor='http://127.0.0.1:4723/wd/hub', desired_capabilities=caps)
 
 # caps = {
 #     'platformName': 'Android',  # 平台
@@ -21,3 +23,11 @@ driver = Remote(command_executor='http://127.0.0.1:4444/wd/hub', desired_capabil
 #     'appActivity': 'com.eagersoft.youzy.youzy.mvvm.ui.launch.LaunchActivity'
 # }
 # driver = Remote(command_executor='http://127.0.0.1:4444/wd/hub', desired_capabilities=caps)
+
+# 等待
+driver.implicitly_wait(10)
+
+driver.find_element(By.CLASS_NAME, 'android.widget.TextView').click()
+# driver.find_element(By.ID, 'com.eagersoft.youzy.youzy:id/iv_logo').click()
+
+driver.quit()
