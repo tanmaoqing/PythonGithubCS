f32c2e1886049d1029a07e86a49b31136c644929
tmq <1738227095@qq.com>
2023-07-31 17:24:05 +0800

    手机屏幕九宫格解锁

--- PytestAndFixtrue/andriod.py
+++ PytestAndFixtrue/andriod.py
@@ -9,15 +9,17 @@ import time
 
 from appium.webdriver import Remote
 from appium.webdriver.common.appiumby import AppiumBy
+from appium.webdriver.common.touch_action import TouchAction
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import ui
 
 caps = {
     'platformName': 'Android',  # 平台
-    'deviceName': 'd86bb73c',  # 设备名 在cmd输入adb devices 可查看   荣耀X30:A8NL6R2106005153  小米:d86bb73c
+    'deviceName': 'YCPDU17510004756',
+    # 设备名 在cmd输入adb devices 可查看   荣耀X30:A8NL6R2106005153  小米:d86bb73c   荣耀6X：YCPDU17510004756
     # 'app': r'D:\androidsdk\android-sdk-windows\build-tools\app-debug.apk',  # 安装包路径
     'noReset': True,
-    'platformVersion': '11'
+    'platformVersion': '8.0.0'  # 安卓手机版本
 }
 driver = Remote(command_executor='http://127.0.0.1:4723/wd/hub', desired_capabilities=caps)
 
@@ -88,12 +90,54 @@ def swipe_huadong():
     driver.swipe(width * 0.1, height * 0.5, width * 0.9, height * 0.5)
 
 
+def install():
+    # 如果存在app删除，不存在则安装
+    if driver.is_app_installed('com.eagersoft.youzy.youzy'):
+        driver.remove_app('com.eagersoft.youzy.youzy')
+        driver.install_app('D:/android APK/com.eagersoft.youzy.youzy_8.4.0_liqucn.com.apk')
+    else:
+        driver.install_app('D:/android APK/com.eagersoft.youzy.youzy_8.4.0_liqucn.com.apk')
+
+
 def swipe_huadong2():
     driver.find_element(By.XPATH,
                         '/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.HorizontalScrollView/android.widget.LinearLayout/androidx.appcompat.app.ActionBar.Tab[2]/android.widget.TextView').click()
     time.sleep(3)
     driver.find_element(By.XPATH,
                         '/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.HorizontalScrollView/android.widget.LinearLayout/androidx.appcompat.app.ActionBar.Tab[3]').click()
+
+
+# 手机屏幕九宫格解锁
+def unlock():
+    size = driver.get_window_size()
+    height = size["height"]
+    width = size["width"]
+    driver.swipe(width * 0.5, height * 0.9, width * 0.5, height * 0.1)
+    time.sleep(1)
+
+    el = driver.find_element(By.ID, 'com.android.systemui:id/lockPatternView')
+    action = el.rect
+    start_x = action['x']
+    start_y = action['y']
+    width = action['width']
+    height = action['height']
+
+    position_1 = {'x': start_x + width * 1 / 6, 'y': start_y + height * 1 / 6}
+    position_2 = {'x': start_x + width * 1 / 2, 'y': start_y + height * 1 / 6}
+    position_3 = {'x': start_x + width * 5 / 6, 'y': start_y + height * 1 / 6}
+    position_4 = {'x': start_x + width * 1 / 6, 'y': start_y + height * 1 / 2}
+    position_5 = {'x': start_x + width * 1 / 2, 'y': start_y + height * 1 / 2}
+    position_6 = {'x': start_x + width * 5 / 6, 'y': start_y + height * 1 / 2}
+    position_7 = {'x': start_x + width * 1 / 6, 'y': start_y + height * 5 / 6}
+    position_8 = {'x': start_x + width * 1 / 2, 'y': start_y + height * 5 / 6}
+    position_9 = {'x': start_x + width * 5 / 6, 'y': start_y + height * 5 / 6}
+
+    touch = TouchAction(driver)
+    touch.press(**position_1).move_to(**position_2).move_to(**position_3).move_to(
+        **position_5).move_to(**position_7).move_to(**position_8).move_to(
+        **position_9).release().perform()
+
+    time.sleep(3)
 
 
 def test_evaluation():
@@ -103,15 +147,10 @@ def test_evaluation():
     # orientation_evaluation()  # 专业定位五合一测评
     # swipe_huadong()  # 左右滑动切换效果
     # swipe_huadong2()  # 通过Xpath 点击切换效果
-    print("包名:{}".format(driver.current_package))
-    print("界面名:{}".format(driver.current_activity))
+    # print("包名:{}".format(driver.current_package))
+    # print("界面名:{}".format(driver.current_activity))
     # driver.start_activity('com.eagersoft.youzy.youzy', '.mvvm.ui.main.Main') # 跳转其他应用
+    # install()  # 如果存在app删除，不存在则安装
+    unlock()  # 手机屏幕九宫格解锁
 
-    # 如果存在app删除，不存在则安装
-    if driver.is_app_installed('com.eagersoft.youzy.youzy'):
-        driver.remove_app('com.eagersoft.youzy.youzy')
-        driver.install_app('D:/android APK/com.eagersoft.youzy.youzy_8.4.0_liqucn.com.apk')
-    else:
-        driver.install_app('D:/android APK/com.eagersoft.youzy.youzy_8.4.0_liqucn.com.apk')
-
-    driver.quit()
+    # driver.quit()
